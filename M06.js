204297da8e9e7c1a655a2cc84fb6a30273186448
ChuckyDeeBobuck <60675523+ChuckyDeeBobuck@users.noreply.github.com>
2020-02-18 14:03:54 -0800

    Update M06.js

--- M06.js
+++ M06.js
@@ -14,4 +14,4 @@ msg += '<b>last modified: </b>' + document.lastModified + '</p>';
 // Create a variable called el to hold the element whose id attribute has a value of footer
 var el = document.getElementById('footer');
 // Write the message into that element
-el.innerHTML = msg;
+//el.innerHTML = msg;
