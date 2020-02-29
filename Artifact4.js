5b943ad050f62ac99d649f11f087bf1b48abbdf7
LAPTOP-878E2B5E\Ray of DOOM <ray.wade@me.com>
2020-02-29 12:19:12 -0800

    updated Artifact4

--- Artifact4.js
+++ Artifact4.js
@@ -1,6 +1,7 @@
 function ValidateForm(){
  var validUsername = false;
  var validUserPassword = false;
+ var validPhone = false;
  var errorMessages ="";  // All the error messages are going to stay in this variable
  /*********** VALIDATES USERNAME ******** */
  //Required field
@@ -8,20 +9,26 @@ function ValidateForm(){
 // You can also use document.getElementById("id-of-field").value
 
 /*********** VALIDATES USERNAME ******** */
- if (myContact.userName.value.length > 0 &&
- myContact.userName.value.length < 15)
+ if (myContact.username.value.length > 0 &&
+ myContact.username.value.length < 15)
  validUsername =true;
  else 
  errorMessages += "<p>The username must be less than 15 characters</p>";
  console.log(validUsername);  
  /*********** VALIDATES PASSWORD ******** */
  if (myContact.userPassword.value==null ||
- myContact.userPassword.value== "" ||
- myContact.userPassword.value.length >15)
+ myContact.password.value== "" ||
+ myContact.password.value.length >15)
  errorMessages += "<p>The password must be less than 15 characters and it is required</p>";
  else
- validUserPassword = true; 
+ validUserPassword = true; p
+
+if (myContact.phone.value.length < 15 && myContact.phone.value.length > 0)
+	validPhone = true;
+else
+	 errorMessages += "<p>The phone number must be less than 15 characters</p>";
+
  document.getElementById("errorMessages").innerHTML = errorMessages;
 // Make sure you return all the boolean variables that are checking each field
- return (validUsername && validUserPassword) ;
+ return (validUsername && validUserPassword && validPhone) ;
 }
